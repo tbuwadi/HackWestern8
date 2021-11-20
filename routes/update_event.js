b6e7babdbf407f52660eaad8d075a442298f4a6a
Elaine Liu <eliu72@uwo.ca>
2021-11-20 17:02:36 -0500

    front and back connected for client login

--- routes/update_event.js
+++ routes/update_event.js
@@ -7,22 +7,21 @@ const router = express.Router();
 const db = require("../db");
 
 // add new attendee when they join
-router.post('/enter-event', enterEvent, async (req, res) => {
+router.post('/enter-event/:name/:email/:event_code', enterEvent, async (req, res) => {
 	res.send(res.locals.attendee);
 });
 
 // add new attendee when they join
 async function enterEvent(req, res, next) { 
-    console.log(req.params);
-    if (!req.body){
-        req.body = {
+    if (!req.params){
+        req.params = {
             event_code: "001",
             name: "Elaine",
             email: "elaineliu7g@gmail.com",
             role: "attendee"
         }
     }
-    const response = db.performCRUD(db.addAttendee, req.body);
+    const response = db.performCRUD(db.addAttendee, req);
     if (response) {
         response.then(function(data) {  
             res.locals.attendee = data;
