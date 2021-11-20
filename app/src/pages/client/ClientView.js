b814011779042c3c97e626370c54e35ba1fdef87
Eng Sung <engxsung@gmail.com>
2021-11-20 15:52:40 -0500

    fixed client view

--- app/src/pages/client/ClientView.js
+++ app/src/pages/client/ClientView.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import View from '../../components/View';
 
 import '../../index.css';
 
 const ClientView = () => {
-  const zoombg = [
+  
+  // delete these test values on demo
+  const zoombgg = [
     "https://raw.githubusercontent.com/tbuwadi/HackWestern8/main/wits-bg/img1.b600c14a.png?token=AHYMZK3OEJ6P3PZJ6MQ2OBDBUKDRS",
     "https://raw.githubusercontent.com/tbuwadi/HackWestern8/main/wits-bg/img2.9f130068.png?token=AHYMZK67N5TFAEOEALAAFADBUKDR6",
     "https://raw.githubusercontent.com/tbuwadi/HackWestern8/main/wits-bg/img3.a5180e90.png?token=AHYMZKZJZQ4RSA6CYKHQPJTBUKDSI",
   ];
 
-  const speakers = [
+  const speakerss = [
     {
       "firstName": "John",
       "lastName": "Smith",
@@ -27,14 +29,24 @@ const ClientView = () => {
       "lastName": "Tim",
       "bio": "@John's cousin"
     }
-  ]
+  ];
+
+  const [title, setTitle] = useState('Loading...');
+  const [desc, setDesc] = useState('Loading...');
+  const [qa, setQa] = useState('https://app.sli.do/event/1yidejwe'); // TODO: Change default value
+  const [zoombg, setZoombg] = useState(zoombgg); // TODO: Change default value
+  const [speakers, setSpeakers] = useState(speakerss); // TODO: Change default value
   
+  useEffect(() => { 
+    console.log('BACKEND INETGRATION HERE')
+  }, []);
+
   return (
     <div className='container' style={{ marginTop: '50px' }}>
-      <h1>Resume Revamp</h1>
-      <h5>description description descriptiondescription</h5>
+      <h1>{title}</h1>
+      <h5>{desc}</h5>
       <br></br>
-      <View speaker={speakers} qna='https://app.sli.do/event/1yidejwe' zoom={zoombg}/>
+      <View speaker={speakers} qna={qa} zoom={zoombg}/>
     </div>
   );
 }
