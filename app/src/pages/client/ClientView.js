cd3a60cf84a5c1619dade9b5199978a2166b6eec
Eng Sung <engxsung@gmail.com>
2021-11-20 10:54:18 -0500

    client view

--- app/src/pages/client/ClientView.js
+++ app/src/pages/client/ClientView.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import '../../index.css';
 
-function ClientView() {
-    return (
-  <h3>ClientView</h3>
-    );
-  }
-  
-  export default ClientView;
-  
+const ClientView = () => {
+  return (
+    <div className='container' style={{ marginTop: '50px' }}>
+      <h1>Resume Revamp</h1>
+      <h5>description description descriptiondescription</h5>
+    </div>
+  );
+}
+
+export default ClientView;
