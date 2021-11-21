d2451386f87c8aa2f7acdb6cc0b270685b7e2c05
Eng Sung <engxsung@gmail.com>
2021-11-21 08:16:28 -0500

    logo

--- app/src/components/AdminSlides/Slides.js
+++ app/src/components/AdminSlides/Slides.js
@@ -3,7 +3,7 @@ import "./TabStyles.css"
 function Slides() {
     return (
         <div className="wrapperSlide">
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTZjshq8U7DgSVtivg4Rri-hdMfB64ARkF4RaAYIaZCjDvMCqd31yEOvmexOcA95b-BzEqtb8a8lslE/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTZjshq8U7DgSVtivg4Rri-hdMfB64ARkF4RaAYIaZCjDvMCqd31yEOvmexOcA95b-BzEqtb8a8lslE/embed?start=false&loop=false&delayms=3000" frameborder="0" width="1423" height="836" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
         </div>
     );
   }
